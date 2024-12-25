a2df3a12e8be9f87ac29d5b81af64599bb5b4838
yeet8900 <vipinreddy89@gmail.com>
2024-12-25 08:46:05 -0700

    dependency error

--- vip/src/App.js
+++ vip/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Button from "@mui/material/Button";
+import { AppBar, IconButton, Tab, Toolbar, Tabs, colors } from "@mui/material";
+import About from "./About.tsx";
+import Projects from "./Projects.tsx";
+import Contact from "./Contact.tsx";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [activeTab, setActiveTab] = useState("about"); // Track the active tab
+
+  // Handle Tab Click
+  const handleTabChange = (event, newValue) => {
+    setActiveTab(newValue); // Update the active tab
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="headerComponent">
+        <AppBar style={{ backgroundColor: "black" }}>
+          <Tabs value={activeTab} onChange={handleTabChange}>
+            <Tab label="About" value="about" sx={{ color: "white" }} />
+            <Tab label="Projects" value="projects" sx={{ color: "white" }} />
+            <Tab label="Contact" value="contact" sx={{ color: "white" }} />
+          </Tabs>
+        </AppBar>
+      </div>
+
+      {activeTab === "about" && <About />}
+      {activeTab === "projects" && <Projects />}
+      {activeTab === "contact" && <Contact />}
+
+      <Button variant="contained" sx={{ backgroundColor: "black" }}>
+        <a href="https://www.google.com" style={{ color: "white" }}>
+          Click me
         </a>
-      </header>
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ vip/src/Projects.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Projects = () => {
+  return <h1>These are my projects</h1>;
+};
+export default Projects;
