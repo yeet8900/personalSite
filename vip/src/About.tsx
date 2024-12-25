d3164d83bddc92ef2b24a24935c26eddef05a8b7
yeet8900 <vipinreddy89@gmail.com>
2024-12-25 08:46:05 -0700

    template

---
+++ vip/src/About.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+const About = () => {
+  return (
+    <div>
+      <h1 className="heading">Hello I'm Vip</h1>
+    </div>
+  );
+};
+export default About;
---
+++ vip/src/Contact.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Contact = () => {
+  return <h1>This is my contact</h1>;
+};
+export default Contact;
